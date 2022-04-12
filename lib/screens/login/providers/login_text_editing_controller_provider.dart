e94229d6628c3c4eccf1eb27179af3a985d467b0
Seven Du <whine.whitish.0u@icloud.com>
2022-04-12 16:56:25 +0800

    🐛 Fixed login text editing controller provider type

--- lib/screens/login/providers/login_text_editing_controller_provider.dart
+++ lib/screens/login/providers/login_text_editing_controller_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final loginAccountTextEditingControllerProvider = Provider.autoDispose(
+final loginAccountTextEditingControllerProvider =
+    ChangeNotifierProvider.autoDispose(
   (Ref ref) => TextEditingController(),
 );
 
-final loginPasswordTextEditingControllerProvider = Provider.autoDispose(
+final loginPasswordTextEditingControllerProvider =
+    ChangeNotifierProvider.autoDispose(
   (Ref ref) => TextEditingController(),
 );
