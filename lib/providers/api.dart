34d2b407f9e3b4ed6d0fbeef89d64b63ff25be48
Seven Du <whine.whitish.0u@icloud.com>
2022-04-12 17:04:52 +0800

    ♻️ Refactored API provider

--- lib/providers/api.dart
+++ lib/providers/api.dart
@@ -2,15 +2,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:socfony/api/api.dart';
 
 import '../configure.dart';
-import 'auth.dart';
+import 'authorization_manager.dart';
+
+AuthenticationTokenReader _createAuthorizationTokenReader(Ref ref) {
+  return () async {
+    final manager = await ref.read(authorizationManagerProvider.future);
+    final access = await manager.getAccessToken();
+
+    return access?.token;
+  };
+}
 
 final apiProvider = Provider(
-  (ref) {
-    final AuthNotifier authNotifier = ref.watch(authProvider.notifier);
-
-    return API(
-      endpoint: Uri.parse(graphqlApiUrl),
-      auth: Authentication(authNotifier.authorizationTokenReader),
-    );
-  },
+  (ref) => API(
+    endpoint: Uri.parse(graphqlApiUrl),
+    auth: Authentication(_createAuthorizationTokenReader(ref)),
+  ),
 );
